938dc87ba80a866de76354c7fbcc92914c926fda
Xavier SETA <xavier.seta@outlook.fr>
2017-01-09 11:46:14 +0100

    Premier comit avec GIT

---
+++ src/main/java/Primefaces/Primefaces_main/App.java
@@ -0,0 +1,9 @@
+package Primefaces.Primefaces_main;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
---
+++ src/test/java/Primefaces/Primefaces_main/AppTest.java
@@ -0,0 +1,12 @@
+package Primefaces.Primefaces_main;
+
+import org.junit.Test;
+
+public class AppTest {
+
+  @Test
+  public void testMain() {
+    App.main(null);
+  }
+
+}
